8897b301ffd68eae6bb52702d4b0683c8fb87b91
Steven McCauley <Steven_McCauley@brown.edu>
2014-05-13 10:43:01 -0400

    Add index to urls.py

--- edit/urls.py
+++ edit/urls.py
@@ -12,6 +12,7 @@ from views import (
 urlpatterns = patterns('',
     url(r'^person/(?P<local_name>[a-z0-9]+)/$', PersonView.as_view(), name='person'),
     url(r'^org/display/(?P<local_name>[a-z0-9]+)/$', UniversityView.as_view(), name='university'),
+    url(r'^index/$', IndexView.as_view(), name='index'),
     url(r'^edit/', ResourceView.as_view(), name='edit'),
     url(r'^service/fast/topic/$', FASTTopicAutocompleteView.as_view(), name='fast-topic'),
     url(r'^service/fast/geo/$', FASTGeoAutocompleteView.as_view(), name='fast-geo'),
